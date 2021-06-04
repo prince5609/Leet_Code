121219637afaf73eb920427010c7c9d6f61bd2f8
prince5609 <prince.tyagi98@gmail.com>
2021-06-04 17:40:44 +0530

    Valid Parentheses

--- Valid_parenthes.py
+++ Valid_parenthes.py
@@ -4,8 +4,8 @@ def isValid(s):
     for char in s:
         if char in my_parentheses_dict:
             top_element = stack.pop() if stack else '#'
-            if my_parentheses_dict[char] != top_element:
-                return False
+            if my_parentheses_dict[char] == top_element:
+                return True
         else:
             stack.append(char)
     return not stack
