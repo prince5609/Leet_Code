32caa889d8838145982644bfb704c65d8600baae
prince5609 <prince.tyagi98@gmail.com>
2021-06-04 17:42:24 +0530

    Decimal To Binary & Vice_Versa

--- Decimal_Binary.py
+++ Decimal_Binary.py
@@ -1,12 +1,12 @@
 def decimal(a):
     i = 0
-    sum = 0
+    sums = 0
     while a != 0:
         rem = a % 10
-        sum += rem * pow(2, i)
+        sums += rem * pow(2, i)
         i += 1
         a = a // 10
-    return sum
+    return sums
 
 
 print(decimal(11001))
@@ -15,14 +15,14 @@ print(decimal(11001))
 def binary(num):
     d = num // 2
     rem = num % 2
-    sum = rem
+    sums = rem
     while d != 1:
         rem = d % 2
         d = d // 2
-        sum = (sum * 10) + rem
-    sum = (sum * 10) + 1
-    sum = str(sum)
-    return sum[::-1]
+        sums = (sums * 10) + rem
+    sums = (sums * 10) + 1
+    sums = str(sums)
+    return sums[::-1]
 
 
 print(binary(25))
