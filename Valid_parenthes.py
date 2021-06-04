78ad2f6e326ce0bed0753b5013063b02e6a5e579
prince5609 <prince.tyagi98@gmail.com>
2021-06-04 17:36:20 +0530

    Valid Parentheses

--- Valid_parenthes.py
+++ Valid_parenthes.py
@@ -1,43 +1,15 @@
-class Solution:
-    def isValid(self, s):
-        stack = []
-        my_parenthes_dict = {")": "(", "}": "{", "]": "["}
-        for char in s:
-            if char in my_parenthes_dict:
-                top_element = stack.pop() if stack else '#'
-                if my_parenthes_dict[char] != top_element:
-                    return False
-            else:
-                stack.append(char)
-        return not stack
-
-obj = Solution()
-print(obj.isValid("([{}])"))
+def isValid(s):
+    stack = []
+    my_parentheses_dict = {")": "(", "}": "{", "]": "["}
+    for char in s:
+        if char in my_parentheses_dict:
+            top_element = stack.pop() if stack else '#'
+            if my_parentheses_dict[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    return not stack
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print(isValid("([{}])"))
+print(isValid("([{{])"))
