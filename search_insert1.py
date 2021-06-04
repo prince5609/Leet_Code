5c8e2828ddd1f4ea2b57282c2de7b57642657f53
prince5609 <prince.tyagi98@gmail.com>
2021-06-04 17:47:24 +0530

    Search & Insert

--- search_insert1.py
+++ search_insert1.py
@@ -1,6 +1,6 @@
-def searchInsert(nums,target):
+def searchInsert(nums, target):
     if target in nums:
-        if len(nums)>=2:
+        if len(nums) >= 2:
             s = 0
             l = len(nums) - 1
             m = int((l - s) / 2)
@@ -21,10 +21,12 @@ def searchInsert(nums,target):
             return 0
 
     else:
-        if nums[-1]<target:
+        if nums[-1] < target:
             return len(nums)
         else:
             for i in range(len(nums)):
-                if nums[i]>target:
+                if nums[i] > target:
                     return i
-print(searchInsert([1],1))
+
+
+print(searchInsert([1], 1))
--- search_insert2.py
+++ search_insert2.py
@@ -1,22 +1,15 @@
-class Solution:
-    def searchInsert(self, nums,target):
-        if target in nums:
+def searchInsert(nums, target):
+    if target in nums:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                return print(i)
+    else:
+        if nums[-1] > target:
             for i in range(len(nums)):
-                if nums[i] == target:
+                if nums[i] > target:
                     return print(i)
         else:
-            if nums[-1] > target:
-                for i in range(len(nums)):
-                    if nums[i] > target:
-                        return print(i)
-            else:
-                return print(len(nums))
-obj = Solution()
-obj.searchInsert([1],1)
+            return print(len(nums))
 
 
-
-
-
-
-
+searchInsert([1], 1)
