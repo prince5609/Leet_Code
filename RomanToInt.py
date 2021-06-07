d44b10e2dd2dae141c01e4d0ab42a2fe782a93aa
prince5609 <prince.tyagi98@gmail.com>
2021-06-07 15:18:36 +0530

    Roman To Int Conversion

--- RomanToInt.py
+++ RomanToInt.py
@@ -1,41 +1,30 @@
-Roman_Conversions = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-    "IV": 4,
-    "IX": 9,
-    "XL": 40,
-    "XC": 90,
-    "CD": 400,
-    "CM": 900
-}
-
-
-def RomanToInt(s):
-    sums = 0
-    n = len(s)
+def roman_to_int(num):
+    Roman_Conversions = {"I": 1,
+                         "V": 5,
+                         "X": 10,
+                         "L": 50,
+                         "C": 100,
+                         "D": 500,
+                         "M": 1000,
+                         "IV": 4,
+                         "IX": 9,
+                         "XL": 40,
+                         "XC": 90,
+                         "CD": 400,
+                         "CM": 900
+                         }
     i = 0
-    if n <= 2:
-        if s[i:i + 2] in Roman_Conversions:
-            return Roman_Conversions[s[i:i + 2]]
-        else:
-            while i < n:
-                sums = sums + Roman_Conversions[s[i]]
-                i = i + 1
-            return sums
-    else:
-        while i < n:
-            if i + 1 < n and s[i:i + 2] in Roman_Conversions:
-                sums = sums + Roman_Conversions[s[i:i + 2]]
-                i = i + 2
-            else:
-                sums = sums + Roman_Conversions[s[i]]
-                i = i + 1
-        return sums
+    sums = 0
+    while i < len(num):
+        if num[i: i + 2] in Roman_Conversions:
+            sums += Roman_Conversions[num[i: i + 2]]
+            i += 2
+        elif num[i] in Roman_Conversions:
+            sums += Roman_Conversions[num[i]]
+            i += 1
+    return sums
 
 
-print(RomanToInt("MCMLXXVI"))
+print(roman_to_int("MCMLXXVI"))
+print(roman_to_int("LVIII"))
+print(roman_to_int("MCMXCIV"))
